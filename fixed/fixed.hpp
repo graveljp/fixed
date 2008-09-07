24eb196f796fe17a139ff7c9db75f712251f5213
joel.riendeau <joel.riendeau@8301e2df-3e56-0410-bad7-75797df71324>
2008-09-07 15:01:49 +0000

    -Removed inlines -Set arithmetic operator arguments to receive const references -Added a templated constructor for other Q types -Started working on comparison operators -Forced m_Frac to an unsigned type, and renamed the types used for m_Magn and m_Frac

--- fixed/fixed.hpp
+++ fixed/fixed.hpp
@@ -17,68 +17,76 @@ template <sint8_t Magnitude, uint8_t Fractional>
 class Q
 {
 public:
-    Q()                   : m_Comp(0) {}
-    Q(const Q& val)       : m_Comp(val.m_Comp) {}
+    Q() : m_Comp(0) {}
+    template <sint8_t M, uint8_t F>
+    explicit Q(const Q<M,F>& val)
+    {
+        m_Comp = (Fractional > F) ?
+                    static_cast<MagnType>(val.m_Comp << (Fractional - F)) :
+                    static_cast<MagnType>(val.m_Comp >> (F - Fractional)) ;
+    }
     template <typename T>
-	Q(T val)              : m_Magn(static_cast<InternalType>(val)), m_Frac(0) {}
+	Q(T val) : m_Magn(static_cast<MagnType>(val)), m_Frac(0) {}
 
 	Q(float val)
     {
-        m_Comp = static_cast<InternalType>(val * (1 << Fractional));
+        m_Comp = static_cast<MagnType>(val * (1 << Fractional));
     }
 
 	Q(double val)
     {
-        m_Comp = static_cast<InternalType>(val * (1 << Fractional));
+        m_Comp = static_cast<MagnType>(val * (1 << Fractional));
     }
 
     template <typename T>
-    inline operator T() const
+    operator T() const
     {
         return static_cast<T>(m_Magn);
     }
 
-    inline operator float() const
+    operator float() const
     {   
-        if (0 == m_Comp)
-            return 0.0f;
         float result = static_cast<float>(m_Comp);
         ((FloatFormat*)&result)->exponent -= Fractional;
-        return result;
+        return (0 == m_Comp) ? 0.0f : result;
     }
 
-    inline bool operator==(const Q& val) const {return m_Comp == val.m_Comp;}
-    inline bool operator!=(const Q& val) const {return m_Comp != val.m_Comp;}
-    inline bool operator< (const Q& val) const {return m_Comp <  val.m_Comp;}
-    inline bool operator<=(const Q& val) const {return m_Comp <= val.m_Comp;}
-    inline bool operator> (const Q& val) const {return m_Comp >  val.m_Comp;}
-    inline bool operator>=(const Q& val) const {return m_Comp >= val.m_Comp;}
+    template <sint8_t M, uint8_t F>
+    bool operator==(const Q<M,F>& val) const
+    {
+        return (m_Magn == val.m_Magn) && (m_Frac == val.m_Frac);
+    }
+    bool operator!=(const Q& val) const {return m_Comp != val.m_Comp;}
+    bool operator< (const Q& val) const {return m_Comp <  val.m_Comp;}
+    bool operator<=(const Q& val) const {return m_Comp <= val.m_Comp;}
+    bool operator> (const Q& val) const {return m_Comp >  val.m_Comp;}
+    bool operator>=(const Q& val) const {return m_Comp >= val.m_Comp;}
 
-    inline Q operator+ (const Q& val) const {Q res; res.m_Comp = m_Comp + val.m_Comp; return res;}
-    inline Q operator+=(const Q& val)       {m_Comp += val.m_Comp; return *this;}
-    inline Q operator- (const Q& val) const {Q res; res.m_Comp = m_Comp - val.m_Comp; return res;}
-    inline Q operator-=(const Q& val)       {m_Comp -= val.m_Comp; return *this;}
-    inline Q operator* (const Q& val) const
+    Q operator+ (const Q& val) const {Q res; res.m_Comp = m_Comp + val.m_Comp; return res;}
+    Q operator+=(const Q& val)       {m_Comp += val.m_Comp; return *this;}
+    Q operator- (const Q& val) const {Q res; res.m_Comp = m_Comp - val.m_Comp; return res;}
+    Q operator-=(const Q& val)       {m_Comp -= val.m_Comp; return *this;}
+    Q operator* (const Q& val) const
     {
         Q res;
         res.m_Comp = (static_cast<MultiplyType>(m_Comp) *
                       static_cast<MultiplyType>(val.m_Comp)) >> Fractional;
         return res;
     }
-    inline Q operator*=(const Q& val)
+    Q operator*=(const Q& val)
     {
         m_Comp =  (static_cast<MultiplyType>(m_Comp) *
                    static_cast<MultiplyType>(val.m_Comp)) >> Fractional;
         return *this;
     }
-    inline Q operator/(const Q& val) const
+    Q operator/(const Q& val) const
     {
         Q res;
         if (val.m_Comp == 0) res.m_Comp = ~val.m_Comp;
         res.m_Comp = ((static_cast<MultiplyType>(m_Comp) << Fractional) / val.m_Comp) >> Fractional;
         return res;
     }
-    inline Q operator/=(const Q& val)
+    Q operator/=(const Q& val)
     {
         if (val.m_Comp == 0)
             m_Comp = ~val.m_Comp;
@@ -108,20 +116,21 @@ public:
         NBits_Frac = Fractional
     };
 
-    typedef typename FixedInteger<NBits,   (Magnitude < 0)>::type InternalType;
+    typedef typename FixedInteger<NBits,   (Magnitude < 0)>::type MagnType;
+    typedef typename FixedInteger<NBits,   false>::type           FracType;
     typedef typename FixedInteger<NBits*2, (Magnitude < 0)>::type MultiplyType;
     typedef typename FixedInteger<(NBits > sizeof(float) ? NBits : sizeof(float)), false>::type FloatCastType;
 
-    inline InternalType Frac() {return m_Frac;}
+    FracType Frac() {return m_Frac;}
 
     union
     {
         struct
         {
-            InternalType m_Frac : NBits_Frac;
-            InternalType m_Magn : NBits_Magn;
+            FracType m_Frac : NBits_Frac;
+            MagnType m_Magn : NBits_Magn;
         };
-        InternalType m_Comp;
+        MagnType m_Comp;
     };
 
 private:
@@ -138,7 +147,7 @@ private:
 };
 
 template <sint8_t Magnitude, uint8_t Fractional>
-inline Q<Magnitude, Fractional> sqrt(const Q<Magnitude, Fractional>& val)
+Q<Magnitude, Fractional> sqrt(const Q<Magnitude, Fractional>& val)
 {
     //if (Magnitude < 0)
     //    return MAX_VAL
@@ -156,7 +165,7 @@ inline Q<Magnitude, Fractional> sqrt(const Q<Magnitude, Fractional>& val)
 }
 
 template <sint8_t Magnitude, uint8_t Fractional>
-inline Q<Magnitude, Fractional> floor(const Q<Magnitude, Fractional>& val)
+Q<Magnitude, Fractional> floor(const Q<Magnitude, Fractional>& val)
 {
     Q<Magnitude, Fractional> ret = val;
     ret.m_Frac = 0;
@@ -164,7 +173,7 @@ inline Q<Magnitude, Fractional> floor(const Q<Magnitude, Fractional>& val)
 }
 
 template <sint8_t Magnitude, uint8_t Fractional>
-inline Q<Magnitude, Fractional> ceil(const Q<Magnitude, Fractional>& val)
+Q<Magnitude, Fractional> ceil(const Q<Magnitude, Fractional>& val)
 {
     Q<Magnitude, Fractional> ret = val;
     if (0 != ret.m_Frac)
