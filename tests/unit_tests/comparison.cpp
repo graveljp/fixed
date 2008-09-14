8111712b7859d76fd4121cac6efa81d7cdf35502
jpgravel <jpgravel@8301e2df-3e56-0410-bad7-75797df71324>
2008-09-14 18:46:03 +0000

    -Removed dependencies on UnitTest++.  Now using Boost::Test instead. -Added default lookup location for Boost library to ../externals/Boost_1_36_0

--- tests/unit_tests/comparison.cpp
+++ tests/unit_tests/comparison.cpp
@@ -1,34 +1,29 @@
-#include "UnitTest++.h"
+#include <boost/test/unit_test.hpp>
 #include "fixed.hpp"
 
-using namespace UnitTest;
-
-namespace
+BOOST_AUTO_TEST_CASE(ComparisonTest)
 {
-    TEST(Comparison)
-    {
-        Q<8, 8> f0(13);
-        Q<8, 8> f1(28);
-        Q<8, 8> f2(1);
-        Q<8, 8> f3(f0);
+    Q<8, 8> f0(13);
+    Q<8, 8> f1(28);
+    Q<8, 8> f2(1);
+    Q<8, 8> f3(f0);
 
-        CHECK(f0 == f3);
-        CHECK(f0 < f1);
-        CHECK(f0 > f2);
-        CHECK(f0 >= f3);
-        CHECK(f0 <= f3);
-        CHECK(f0 != f1);
+    BOOST_CHECK(f0 == f3);
+    BOOST_CHECK(f0 < f1);
+    BOOST_CHECK(f0 > f2);
+    BOOST_CHECK(f0 >= f3);
+    BOOST_CHECK(f0 <= f3);
+    BOOST_CHECK(f0 != f1);
 
-        Q<-8, 8> f0n(13);
-        Q<-8, 8> f1n(-28);
-        Q<-8, 8> f2n(-1);
-        Q<-8, 8> f3n(f0n);
+    Q<-8, 8> f0n(13);
+    Q<-8, 8> f1n(-28);
+    Q<-8, 8> f2n(-1);
+    Q<-8, 8> f3n(f0n);
 
-        CHECK(f0n == f3n);
-        CHECK(f0n > f1n);
-        CHECK(f2n < f0n);
-        CHECK(f0n >= f3n);
-        CHECK(f0n <= f3n);
-        CHECK(f1n != f2n);
-    }
+    BOOST_CHECK(f0n == f3n);
+    BOOST_CHECK(f0n > f1n);
+    BOOST_CHECK(f2n < f0n);
+    BOOST_CHECK(f0n >= f3n);
+    BOOST_CHECK(f0n <= f3n);
+    BOOST_CHECK(f1n != f2n);
 }
--- tests/unit_tests/construction.cpp
+++ tests/unit_tests/construction.cpp
@@ -1,33 +1,28 @@
-#include "UnitTest++.h"
+#include <boost/test/unit_test.hpp>
 #include "fixed.hpp"
 
-using namespace UnitTest;
-
-namespace
+BOOST_AUTO_TEST_CASE(ConstructionTest)
 {
-	TEST(Construct_Cast)
-	{
-        Q<8, 8> f0;
-        CHECK_EQUAL(static_cast<float>(f0), 0.0f);
+    Q<8, 8> f0;
+    BOOST_CHECK_EQUAL(static_cast<float>(f0), 0.0f);
 
-        float ft = 3.75f;
-		Q<8, 8> f1(ft);
-        CHECK_EQUAL(static_cast<float>(f1), ft);
+    float ft = 3.75f;
+    Q<8, 8> f1(ft);
+    BOOST_CHECK_EQUAL(static_cast<float>(f1), ft);
 
-        ft = -3.75f;
-		Q<-8, 8> f1n(ft);
-        CHECK_EQUAL(static_cast<float>(f1n), ft);
+    ft = -3.75f;
+    Q<-8, 8> f1n(ft);
+    BOOST_CHECK_EQUAL(static_cast<float>(f1n), ft);
 
-        Q<-8, 8> f2(f1n);
-        CHECK_EQUAL(static_cast<float>(f2), ft);
+    Q<-8, 8> f2(f1n);
+    BOOST_CHECK_EQUAL(static_cast<float>(f2), ft);
 
-        uint16_t u = 45;
-        Q<8, 8> f3(u);
-        CHECK_EQUAL(static_cast<uint16_t>(f3), u);
+    uint16_t u = 45;
+    Q<8, 8> f3(u);
+    BOOST_CHECK_EQUAL(static_cast<uint16_t>(f3), u);
 
-        sint16_t s = -33;
-        Q<-8, 8> f4(s);
-        CHECK_EQUAL(static_cast<sint16_t>(f4), s);
-        CHECK_EQUAL(static_cast<float>(f4), -33.f);
-    }
+    sint16_t s = -33;
+    Q<-8, 8> f4(s);
+    BOOST_CHECK_EQUAL(static_cast<sint16_t>(f4), s);
+    BOOST_CHECK_EQUAL(static_cast<float>(f4), -33.f);
 }
